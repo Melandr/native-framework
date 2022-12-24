7dbd4e0bc7ae07b177fb251b99c5ffe15268cb8d
Melandr <melnychenko.a@gmail.com>
2022-12-24 20:31:20 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import "../node_modules/materialize-css/dist/css/materialize.min.css";
+import "materialize-css";
+
+console.log("App is working!");
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const webpack = require("webpack");
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    context: path.join(__dirname, "src"),
+
+    entry: {
+        main: "./index.js",
+    },
+
+    output: {
+        filename: "bundle.js",
+        path: path.join(__dirname, "dist"),
+    },
+
+    devtool: "eval",
+
+    devServer: {
+        port: 4200,
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: "Native Framework",
+            template: "./index.html",
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"],
+                include: /node_modules/,
+            },
+        ],
+    },
+};
